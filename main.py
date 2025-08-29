01824ce884150204505fbad7ffabf76ce1bd21b5
MaitriKoyani <program4192@gmail.com>
2025-08-29 23:04:21 +0530

    last commit from asus

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-from fastapi import FastAPI, Request, Response, Form
+from fastapi import FastAPI, Request, Response, Form, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy import create_engine, Column, String
 from sqlalchemy.orm import sessionmaker, declarative_base
-import uuid  # For generating unique session IDs
+import uuid  
 import random
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-# SQLite Database
 DATABASE_URL = "sqlite:///./sessions.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Define Session Model
 class UserSession(Base):
     __tablename__ = "sessions"
     session_id = Column(String, primary_key=True, index=True)
     username = Column(String)
     room_code = Column(String)
 
-Base.metadata.create_all(bind=engine)  # Create the table
+Base.metadata.create_all(bind=engine) 
 
-# Function to save session to DB
 def save_session(session_id, username, room_code):
     db = SessionLocal()
     user_session = UserSession(session_id=session_id, username=username, room_code=room_code)
@@ -31,7 +28,6 @@ def save_session(session_id, username, room_code):
     db.commit()
     db.close()
 db = SessionLocal()
-# Function to get session from DB
 def get_session(session_id):
     db = SessionLocal()
     session_data = db.query(UserSession).filter_by(session_id = session_id).first()
@@ -41,23 +37,28 @@ def get_session(session_id):
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     
-    return templates.TemplateResponse("home.html", {"request": request})
-
-@app.post("/create-room")
-def create_room(response: Response, username: str = Form(...), room_code: str = Form(...)):
-    session_id = str(uuid.uuid4())  # Generate a unique session ID
-    # Add user to the room
+    return templates.TemplateResponse("welcome.html", {"request": request})
+@app.post("/", response_class=HTMLResponse)
+def check_name(request: Request, username: str = Form(...)):
     if db.query(UserSession).filter_by(username = username).first():
         print('hi')
-        return RedirectResponse(url="/model", status_code=404)
+        return templates.TemplateResponse("welcome.html", {"status": "Username already exists", "request": request}) 
+    return templates.TemplateResponse("home.html", {"request": request, "name": username})
+@app.post("/create-room/{username}")
+def create_room(response: Response,request: Request, username: str, room_code: str = Form(...)):
+    print(room_code,'room_code')
+    session_id = str(uuid.uuid4())  
+    
     if room_code:
         room_code = int(room_code)
         ses = db.query(UserSession).filter_by(room_code = room_code).first()
-        if not ses:
-            return RedirectResponse(url="/", status_code=404)
+        if not ses:                                      
+            print('not')
+            return templates.TemplateResponse("home.html", {"status": "Room code does not exist", "request": request, "name": username})
+
     if not room_code :
         room_code = random.randint(100000, 999999)
-        
+
     save_session(session_id, username, room_code)
     
     response = RedirectResponse(url=f"/room/", status_code=303)
@@ -74,7 +75,7 @@ def room(request: Request):
     room_code=int(room_code)
     
     if not user_data:
-        return RedirectResponse(url="/")  # Redirect if no session exists
+        return RedirectResponse(url="/")  
     users = db.query(UserSession).filter_by(room_code=room_code).all()
     users_in_room = []
     for user in users:
@@ -87,5 +88,5 @@ def logout(response: Response, request: Request):
     db.query(UserSession).filter_by(session_id=session_id).delete()
     db.commit()
     response = RedirectResponse(url="/", status_code=302)
-    response.delete_cookie("session_id")  # Remove cookie
+    response.delete_cookie("session_id")  
     return response
